9a199605c6dca149ea348153411f445bbd57fc0e
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-06-16 09:24:32 -0500

    Create plantilla.cpp

---
+++ plantilla.cpp
@@ -0,0 +1,71 @@
+/*
+	EXPLICAR QUE HACE PROGRAMA.
+	AUTOR:
+	COMPILAR: 
+		
+		g++ -Wall -o NOmbre_EJecutable fuente.cpp `pkg-config --cflags --libs opencv`
+
+*/
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+static void SiRatonMueve(int evt, int x, int y, int flags, void* param){
+	switch(evt)
+	{
+		case CV_EVENT_LBUTTONDOWN :
+
+				break;
+		default : 
+				break;
+	}
+
+}
+
+
+int main( void ){
+
+	
+	
+
+
+
+return(0);
+}
+
+//  Mat imagen(w,w,CV_8UC3,Scalar(0, 255, 0));
+//	imshow("NombreVentana", NombreMatriz);
+//	waitKey( 0 );
+//  VideoCapture cam(0);
+//  if(cam.isOpened()) return -1;
+//  cam >> video;
+/*
+
+    cvtColor(video, video, CV_BGR2HSV, 0);	
+
+		inRange(video,
+		Scalar(0,0,50),
+		Scalar(70,150,200),
+		oediv);
+
+    NOmbre_Matriz_Guardar_Imagen = imread("imagen1.png");
+    
+    imwrite("HOla.jpg", video);
+    
+    resize(video, temporal, Size(250,150), 0,0,INTER_LINEAR);
+    
+    GaussianBlur(temporal, blur, Size(5, 5), 2, 2);
+    
+    Rect R1(Point(10,10), Size(200,100));
+    
+    createTrackbar("SatMin", "ControlCasa-V0.111", &SatMin, 255);
+    
+    cvSetMouseCallback("ControlCasa-V0.111",SiRatonMueve, &temp );
+    
+    temporal.copyTo(plantilla(R4));
+
+*/
+
